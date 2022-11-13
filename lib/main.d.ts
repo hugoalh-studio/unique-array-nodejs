b9a58a6deb26d480da58b1ad6a2f0f7deb992272
hugoalh <32359235+hugoalh@users.noreply.github.com>
2022-11-13 13:37:50 +0800

    20221113A

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -2,11 +2,10 @@ export = uniqueArray;
 /**
  * @function uniqueArray
  * @description Return unique array elements by ignore all of the duplicated elements.
- * @param {unknown[]} item Array that need to have unique elements.
+ * @template T
+ * @param {T[]} item Array that need to have unique elements.
  * @param {boolean} [ignoreReferences=false] Whether to compare objects without compare their reference points.
- * @returns {unknown[]} An array with unique elements.
- * @throws {TypeError} Argument `item` is not type of array.
- * @throws {TypeError} Argument `ignoreReferences` is not type of boolean.
+ * @returns {T[]} An array with unique elements.
  */
-declare function uniqueArray(item: unknown[], ignoreReferences?: boolean): unknown[];
+declare function uniqueArray<T>(item: T[], ignoreReferences?: boolean): T[];
 //# sourceMappingURL=main.d.ts.map
